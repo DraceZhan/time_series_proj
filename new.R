88df92a742bab8868a2c397aabd9426240e9c61b
DraceZhan <Dracezhan@gmail.com>
2018-11-10 20:12:20 -0500

    new updates

--- new.R
+++ new.R
@@ -5,14 +5,50 @@ library(tidyr)
 library(forecast)
 library(dplyr)
 library(astsa)
+library(tseries)
+library(TSA)
 
-air_reserve = read_csv('../random/mta.csv')
+air_reserve = read_csv("C:/Users/Drace/Desktop/time_series_proj-master/air_visit_data.csv")
+air_reserve = air_reserve %>% group_by(as.Date(air_reserve$visit_date)) %>% summarise(visitors = sum(visitors))
+colnames(air_reserve)[1] = 'Date'
+BoxCox.lambda(air_reserve$visitors)
+air_reserve$bc_visitors = BoxCox(air_reserve$visitors, lambda = 0)
+air_reserve_xts = xts(air_reserve, order.by = air_reserve$Date)
 
-air_reserve %>% group_by(air_store_id) %>% tally(sort = T) %>% top_n(1)
+#Visuals
+dygraph(air_reserve_xts[,c(2,3)]) %>% dySeries('visitors', label = 'Total Visitors')  %>%dySeries(
+  'bc_visitors', label = 'Box-Cox Transformed Total Visitors') %>% dyRangeSelector(
+    height = 20) %>%dyOptions(
+    logscale=F) %>%
+  dyHighlight(highlightSeriesOpts = list(strokeWidth = 1))
+  
+  
+dygraph(air_reserve_xts[,c(3)]) %>% dySeries(
+    'bc_visitors', label = 'Box-Cox Transformed Total Visitors') %>%dyRangeSelector(height = 20) %>%
+    dyHighlight(highlightSeriesOpts = list(strokeWidth = 1)) %>% dyOptions(logscale=F)
 
-air_reserve_top_rest = subset(air_reserve, air_store_id == (
-  air_reserve %>% group_by(air_store_id) %>% tally(sort = T) %>% top_n(1))[[1]])
+dygraph(visitors, main = "Rolling Five Days") %>% 
+  dyRoller(rollPeriod = 5)
 
-top_rest_xts = xts(
-  air_reserve_top_rest$reserve_visitors, 
-  order.by=as.POSIXct(air_reserve_top_rest$visit_datetime))
+#acf/pacf plots
+air_reserve_ts = ts(air_reserve)
+acf(air_reserve_ts[,c(3)])
+pacf(air_reserve_ts[,c(3)])
+
+#acf shows weekly seasonality, we can further verify this below
+#The power spectrum is the discrete Fourier transform of the autocovariance function of an appropriately smoothed version of the original series, we use a periodogram to examine if there is any seasonality here
+
+p = periodogram(
+  air_reserve_ts[,c(2)],log='yes',plot=TRUE,ylab="Periodogram", xlab="Frequency",lwd=2)
+dd = data.frame(freq=p$freq, spec=p$spec)
+order = dd[order(-dd$spec),]
+top5 = head(order, 5)
+time_conv = 1/top5$freq
+time_conv
+
+#seasonality was detected for increments of 120 days, roughly 1/3rd of a year. Also weekly seasonality was detected as well
+
+#taking difference at lag 7
+ts_diff = diff(air_reserve_ts, differences = 1, lag = 7)
+acf(ts_diff[,c(3)])
+pacf(ts_diff[,c(3)])
